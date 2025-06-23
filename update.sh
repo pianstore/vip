eeefff232729e1ce4a51f1b200b8067aa7e60311
PIAN STORE <stoksipu@gmail.com>
2025-06-24 02:16:46 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -49,6 +49,6 @@ fun_bar 'res1'
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 read -n 1 -s -r -p "Press [ Enter ] to back on menu"
-menu
+welcome
 
 ###########- COLOR CODE -##############
